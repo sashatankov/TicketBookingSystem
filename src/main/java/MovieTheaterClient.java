c26e703a437c21135744f4cff5a57ded7052c726
sashatankov <isasha96@gmail.com>
2020-08-03 11:31:00 +0300

    threading and sockets added to server and client

--- src/main/java/MovieTheaterClient.java
+++ src/main/java/MovieTheaterClient.java
@@ -1,6 +1,31 @@
+import java.io.IOException;
+import java.net.Socket;
 
+public class MovieTheaterClient implements Runnable {
 
-public class MovieTheaterClient {
+    public static final String DEFAULT_HOST = "localhost";
+    public static final int DEFAULT_PORT = 5000;
+    private Socket socket;
 
+    public MovieTheaterClient(Socket socket) {
+        this.socket = socket;
+    }
 
+    @Override
+    public void run() {
+
+        while(true) {
+            // TODO the logic of the interaction with the server
+        }
+    }
+
+    public static void main(String[] args) {
+
+        try(Socket socket = new Socket(DEFAULT_HOST, DEFAULT_PORT)) {
+
+        } catch(IOException e) {
+            System.err.println("NETWORK ERROR");
+            System.exit(2);
+        }
+    }
 }
--- src/main/java/MovieTheaterServer.java
+++ src/main/java/MovieTheaterServer.java
@@ -1,6 +1,32 @@
-
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class MovieTheaterServer {
 
+    private static final int DEFAULT_PORT = 5000;
+    private Socket socket;
     private MovieTheaterModel model;
+
+    public MovieTheaterServer(Socket socket) {
+
+        this.model = new MovieTheaterModel();
+        this.socket = socket;
+
+    }
+
+
+    public static void main(String[] args) {
+
+        try(ServerSocket serverSocket = new ServerSocket(DEFAULT_PORT)) {
+
+            while (true) {
+                new Thread(new MovieTheaterClient(serverSocket.accept())).start();
+            }
+
+        } catch (IOException e) {
+            System.err.println("500 INTERNAL SERVER ERROR");
+            System.exit(3);
+        }
+    }
 }
